11ec6055f90f7c10476d168c91476f5a0dbe13de
Adrian Di Paulo <adridpf@hotmail.com>
2021-04-20 14:58:30 -0300

    Listo al 20_04 avances

--- app/src/main/java/com/terobyte/appmedicamentos/UsuariosActivity.java
+++ app/src/main/java/com/terobyte/appmedicamentos/UsuariosActivity.java
@@ -4,9 +4,17 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
+import com.terobyte.appmedicamentos.adapters.UsuarioAdapter;
+import com.terobyte.appmedicamentos.entidades.Usuarios;
+import com.terobyte.appmedicamentos.models.UsuarioViewModel;
+import com.terobyte.appmedicamentos.repsitories.UsuariosFactory;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,11 +23,21 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class UsuariosActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private UsuarioViewModel usuarioViewModel;
+    public static final int CODE_USUARIOS=1;
+    public static final int UPDATE_CODE_USUARIOS=2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usuarios);
+        RecyclerView recyclerView= findViewById(R.id.recyclerViewUsuarios);
+        final UsuarioAdapter adapter= new UsuarioAdapter(new UsuarioAdapter.usuarioDiff());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        usuarioViewModel = new ViewModelProvider(this,new UsuariosFactory(getApplication())).get(UsuarioViewModel.class);
+        usuarioViewModel.verUsuarios().observe(this, Usuarios->{adapter.submitList(Usuarios);});
+        FloatingActionButton fab= findViewById(R.id.AgregarBotonFloatUsuarios);
 
         MaterialToolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
