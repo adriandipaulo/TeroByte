8222e5328d0eccda6de2cc36061a1ff52520a5b5
Adrian Di Paulo <adridpf@hotmail.com>
2021-04-25 03:11:53 -0300

    Actualiza para intent solo envio de notifiacion

---
+++ app/src/main/java/com/terobyte/appmedicamentos/alarmManager/AlarmReceiver.java
@@ -0,0 +1,51 @@
+package com.terobyte.appmedicamentos.alarmManager;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+
+import androidx.core.app.NotificationCompat;
+
+import com.terobyte.appmedicamentos.MainActivity;
+import com.terobyte.appmedicamentos.R;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    public static final String EXTRA_ID = "com.terobyte.appmedicamentos.id";
+    public static final String EXTRA_NOM = "com.terobyte.appmedicamentos.nombre";
+    public static final String EXTRA_HOR = "com.terobyte.appmedicamentos.hora";
+    public static final String EXTRA_DOS = "com.terobyte.appmedicamentos.dosis";
+    public static final String EXTRA_USU = "com.terobyte.appmedicamentos.usuario";
+    public static final String EXTRA_FORMATOHORA = "com.terobyte.appmedicamentos.formatohora";
+    public static String nombreMed;
+    public static String usuarioMed;
+    public static String presMed;
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        nombreMed=intent.getStringExtra(EXTRA_NOM);
+        usuarioMed=intent.getStringExtra(EXTRA_USU);
+        presMed=intent.getStringExtra(EXTRA_DOS);
+        showNotification(context);
+
+    }
+
+    public void showNotification(Context context) {
+        Intent i = new Intent(context,MainActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(context, 0, i, 0);
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context,"Canal001")
+                .setSmallIcon(R.drawable.ic_baseline_medical_services_24)
+                .setColor(Color.RED)
+                .setContentTitle(usuarioMed)
+                .setContentText("Tomar: " + presMed + " de: " + nombreMed);
+        mBuilder.setContentIntent(pi);
+        mBuilder.setDefaults(Notification.DEFAULT_SOUND);
+        mBuilder.setAutoCancel(true);
+        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(0, mBuilder.build());
+    }}
+
